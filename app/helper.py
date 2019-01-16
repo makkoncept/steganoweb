50e298850e9886e82bebbce90f0b5d4ba4de4ea4
Mayank Nader <mayank.nader11@gmail.com>
2019-01-16 22:41:38 +0530

    add basic structure

---
+++ app/helper.py
@@ -0,0 +1,11 @@
+from stegano import lsb
+
+
+def encode(img_path, message, save_path):
+    secret = lsb.hide(img_path, message)
+    secret.save(save_path)
+
+
+def decode(img_path):
+    message = lsb.reveal(img_path)
+    return message
