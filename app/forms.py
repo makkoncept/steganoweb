58c961b67c1f71414dbeafa92ad31839ad0574c5
Mayank Nader <mayank.nader11@gmail.com>
2019-01-17 11:28:26 +0530

    add templates and forms

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import IntegerField, StringField
+from wtforms.validators import NumberRange, Length, DataRequired
+
+
+class SecretMessageForm(FlaskForm):
+    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['png'])])
+    message = StringField('Secret message', validators=[DataRequired(), Length(1, 100)])
+    palette_height = IntegerField('Palette Height', validators=[NumberRange(1, 10)])
+    palette_outline_width = IntegerField('Palette Outline Width', validators=[NumberRange(1, 40)])
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from app.forms import SecretMessageForm
+from flask import render_template
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "hello"
+    form = SecretMessageForm()
+    if form.validate_on_submit:
+        return "yes"
+    return render_template("index.html", form=form)
+
+
+@app.route("/decode")
+def decode():
+    form = SecretMessageForm()
+    return render_template("index.html", form=form)
