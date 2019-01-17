c622c528bb8e8819f4fca65f16e455ee85cd8fad
Mayank Nader <mayank.nader11@gmail.com>
2019-01-17 13:15:41 +0530

    add logic to encode message into image

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import NumberRange, Length, DataRequired
 
 
 class SecretMessageForm(FlaskForm):
-    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['png'])])
-    message = StringField('Secret message', validators=[DataRequired(), Length(1, 100)])
-    palette_height = IntegerField('Palette Height', validators=[NumberRange(1, 10)])
-    palette_outline_width = IntegerField('Palette Outline Width', validators=[NumberRange(1, 40)])
+    photo = FileField("Upload Image", validators=[FileRequired(), FileAllowed(["png"])])
+    message = StringField("Secret message", validators=[DataRequired(), Length(1, 100)])
+    # palette_height = IntegerField('Palette Height', validators=[NumberRange(1, 10)])
+    # palette_outline_width = IntegerField('Palette Outline Width', validators=[NumberRange(1, 40)])
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,78 @@
+import os
+import uuid
+
 from app import app
 from app.forms import SecretMessageForm
-from flask import render_template
+from app.helper import encode
+from flask import render_template, redirect, url_for, request, session
+from werkzeug.utils import secure_filename
+from PIL import Image
 
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     form = SecretMessageForm()
-    if form.validate_on_submit:
-        return "yes"
+    if form.validate_on_submit():
+        print("hahah")
+        # print(hex_to_rgb(request.form.get('palette_outline_color')))
+        f = form.photo.data
+        filename = secure_filename(f.filename)
+        _, ext = os.path.splitext(filename)
+        filename = uuid.uuid4().hex + ext
+        print(filename)
+        # f, pal2, hex_codes = get_colors(f, palette_length_div=form.palette_height.data, outline_width=form.palette_outline_width.data,
+        #                outline_color=hex_to_rgb(request.form.get('palette_outline_color')))
+        path = os.path.join(app.root_path, "static/images", filename)
+        save_path = os.path.join(app.root_path, "static/images", "hid" + filename)
+        # path2 = os.path.join(app.root_path, 'static/images', "pal"+filename)
+        message = form.message.data
+        f.save(path)
+        f = Image.open(path)
+        encode(path, message, save_path)
+        # pal2.save(path2)
+        print(f.height)
+
+        return redirect(
+            url_for("picture", name="hid" + filename, height=f.height, width=f.width)
+        )
+
+    # return render_template('index.html', form=form, src='default')
     return render_template("index.html", form=form)
+
+
+@app.route("/picture/<name>/<height>/<width>")
+def picture(name, height, width):
+    src = url_for("static", filename="images/" + name)
+    # save_path = url_for("static", filename="images/" + "hid" + name)
+    # encode(path, session.get("message"), save_path)
+    # src2 = url_for("static", filename="images/" + "pal" + name)
+    height, width = img_tag_size(int(height), int(width))
+    return render_template(
+        "picture.html",
+        src=src,
+        # src2=src2,
+        height=height,
+        width=width,
+        # hex_codes=session.get("hex_codes"),
+    )
+
+
+def img_tag_size(height, width):
+    if height < 850 and width < 850:
+        return height, width
+    else:
+        while height > 850 and width > 850:
+            height = int(height / 2)
+            width = int(width / 2)
+        return height, width
 
 
 @app.route("/decode")
 def decode():
     form = SecretMessageForm()
     return render_template("index.html", form=form)
+
+
+# @app.errorhandler(413)
+# def error413(e):
+#     return render_template("413.html"), 413
