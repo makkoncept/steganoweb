45706751ec439d1c5d523efbaf053a54bd201042
Mayank Nader <mayank.nader11@gmail.com>
2020-09-18 21:39:12 +0530

    randomise the fallback secret key

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 
-class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'abadsecret'
+class Config:
+    SECRET_KEY = (
+        os.environ.get("SECRET_KEY") or "ujxnPZCAnfoAglcte8AsPDRZCZkmDOtde2TD2sL4"
+    )
