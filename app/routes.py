16ba5ce4e126d95803f8e2ec44fdc9757c431c29
Mayank Nader <mayank.nader11@gmail.com>
2020-10-12 23:25:07 +0530

    Add "download image" button
    Also, remove img_tag_size() because adjusting the height and width on
    the server is not needed anymore.

--- app/routes.py
+++ app/routes.py
@@ -21,28 +21,15 @@ def index():
             default_image = Image.open(DEFAULT_FILE_PATH)
             processed_filename = encode_message(default_image, message)
 
-        return redirect(
-            url_for("picture", name=processed_filename, height=500, width=500)
-        )
+        return redirect(url_for("picture", image_name=processed_filename))
 
     return render_template("index.html", form=form)
 
 
-@app.route("/picture/<name>/<height>/<width>")
-def picture(name, height, width):
-    src = url_for("static", filename="images/" + name)
-    height, width = img_tag_size(int(height), int(width))
-    return render_template("picture.html", src=src, height=height, width=width)
-
-
-def img_tag_size(height, width):
-    if height < 850 and width < 850:
-        return height, width
-    else:
-        while height > 850 and width > 850:
-            height = int(height / 2)
-            width = int(width / 2)
-        return height, width
+@app.route("/picture/<image_name>")
+def picture(image_name):
+    src = url_for("static", filename="images/" + image_name)
+    return render_template("picture.html", src=src)
 
 
 @app.route("/decode", methods=["GET", "POST"])
