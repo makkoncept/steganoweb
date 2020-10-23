aa55078047eb392eea452f99abfa48ca6b64fc3b
Mayank Nader <mayank.nader11@gmail.com>
2020-10-24 02:15:32 +0530

    add client side form validation

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,9 +1,4 @@
-const imageUpload = document.querySelector(".image-upload");
-const imageInput = document.querySelector(".image-input");
-const imageNameDiv = document.querySelector(".image-name");
 const legalFileTypes = ["image/png", "image/jpg", "image/jpeg"];
-
-imageUpload.addEventListener("change", handleImageUpload);
 
 function handleImageUpload(event) {
   console.log(event.target.files[0]);
@@ -11,10 +6,9 @@ function handleImageUpload(event) {
   const fileType = event.target.files[0].type;
   if (FileSize > 2) {
     alert("File size exceeds 2 MB");
+    event.target.value = ''
   } else if (!legalFileTypes.includes(fileType)) {
     alert("Only file of type 'jpeg', 'jpg', or 'png' allowed");
-  } else {
-    const imageName = event.target.files[0].name;
-    imageNameDiv.innerText = imageName;
-  }
+    event.target.value = ''
+  } 
 }
